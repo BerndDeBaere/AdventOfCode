e840852bea2a434484ea6619a77c459f8b3ff2a2
BerndDeBaere <bernd.debaere@outlook.be>
2024-12-03 12:03:57 +0100

    Day3

---
+++ AdventOfCode/Day3.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode;
+
+public class Day3
+{
+    public void Part1()
+    {
+        int totalValue = 0;
+        var line = File.ReadAllText("Input/Day3.txt");
+        var matches = Regex.Matches(line, @"mul\((\d{1,3}),(\d{1,3})\)");
+        foreach (Match match in matches.ToList())
+        {
+            totalValue += (int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value));
+        }
+
+        Console.WriteLine(totalValue);
+    }
+
+    public void Part2()
+    {
+        int totalValue = 0;
+        var line = File.ReadAllText("Input/Day3.txt");
+        var matches = Regex.Matches(line, @"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)");
+        bool enabled = true;
+        foreach (Match match in matches.ToList())
+        {
+            if (match.Value == "do()")
+                enabled = true;
+            else if (match.Value == "don't()")
+                enabled = false;
+            else if (enabled)
+                totalValue += (int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value));
+        }
+
+        Console.WriteLine(totalValue);
+    }
+}
